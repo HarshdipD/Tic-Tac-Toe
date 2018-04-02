2b86da2cc208754a33347c806f3fa88d4974196b
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2018-04-02 15:09:49 -0400

    Changed the way it will work
    First I tried to play it with computer vs Pc, but I think I should start with 2 players first

--- TicTacToe.c
+++ TicTacToe.c
@@ -2,55 +2,33 @@
 #include <stdlib.h>
 #include <time.h>
 
+void PrintGrid();
 
-void Computer(char array[3][3]);
+  char TicTacToe[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
 
 int main(){
 
-  int row_input, col_input;
-
-  printf("\nTicTacToe!\n");
-  printf("Your symbol is 'X'\n");
-
-  char TicTacToe[3][3] = { {'1', '2', '3'},
-                            {'4', '5', '6'},
-                            {'7', '8', '9'}};
-
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      printf("%c ", TicTacToe[i][j]);
-    }
-    printf("\n");
-  }
-  printf("Enter where you want to put your X in the form \"row,col\":  \n");
-  scanf("%d,%d\n", &row_input, &col_input);
-
-  TicTacToe[row_input-1][col_input-1] = 'X';
-
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      printf("%c ", TicTacToe[i][j]);
-    }
-    printf("\n");
-  }
-  Computer(TicTacToe);
-
-
-
+  PrintGrid();
+  
   return 0;
 }
 
-void Computer(char array[3][3]){
+void PrintGrid(){
+  printf("TicTacToe!\n");
+  printf("Player 1: X\tvs\tPlayer 2: O\n");
 
-  time_t t;
-  srand((unsigned) time(&t));
-  int random = rand() % 9;
+  printf("     |     |     \n");
+  printf("  %c  |  %c  |  %c  \n", TicTacToe[1], TicTacToe[2], TicTacToe[3]);
+  printf("_____|_____|_____\n");
+  printf("     |     |     \n");
 
-  for(int i = 0; i < 3; i++){
-    for(int j = 0; j < 3; j++){
-      if(array[i][j] == random){
-        array[i-1][j-1] = 'O';
-      }
-    }
-  }
+  printf("  %c  |  %c  |  %c \n", TicTacToe[4], TicTacToe[5], TicTacToe[6]);
+
+  printf("_____|_____|_____\n");
+  printf("     |     |     \n");
+
+  printf("  %c  |  %c  |  %c \n", TicTacToe[7], TicTacToe[8], TicTacToe[9]);
+
+  printf("     |     |     \n\n");
 }
