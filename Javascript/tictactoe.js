a4b3fe84a9e4a9361b99155ca18e2dce2a3fa578
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2018-04-05 16:05:18 -0400

    Create tictactoe.js

---
+++ Javascript/tictactoe.js
@@ -0,0 +1,9 @@
+var mark = 1;
+
+function change() {
+    document.getElementById("1").innerHTML = mark;
+     document.getElementById('1').style.cssText
+     =
+     'font-size:63px;';
+     mark++;
+}
