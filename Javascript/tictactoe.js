68985ac36ab9c79e1a36e2486aa5baa8ebad1a6a
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2018-04-08 22:53:25 -0400

    Update tictactoe.js

--- Javascript/tictactoe.js
+++ Javascript/tictactoe.js
@@ -2,7 +2,7 @@ var mark = 1;
 
 function change() {
     document.getElementById("1").innerHTML = mark;
-     document.getElementById('1').style.cssText
+     document.getElementById("1").style.cssText
      =
      'font-size:63px;';
      mark++;
