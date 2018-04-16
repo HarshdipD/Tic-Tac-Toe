18904767772ed5db67f536e30f6fdac323cf1772
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2018-04-16 16:03:46 -0400

    Update TicTacToe.py

--- Python/TicTacToe.py
+++ Python/TicTacToe.py
@@ -25,5 +25,49 @@ def PrintGrid():
 
 
 ##Main Function starts
-PrintGrid()
-player = (player%2) ? 1 : 2; #Player number changes after each turn
+player = player%2 #Player number changes after each turn
+i = -1
+while i == -1:
+    PrintGrid()
+    if player%2:
+        player = 1
+    else:
+        player = 2
+    #Takes input from the user to fill in the grid
+    print("player ", end="")
+    print(player, end="")
+    choice = input(", enter a number: ")
+    mark = player
+    if player == 1:
+        mark = 'X'
+    else:
+        mark = 'O'
+    if(choice == 1 and TicTacToe[1] == '1'):
+        TicTacToe[1] = ['mark']
+
+    elif(choice == 2 and TicTacToe[2] == '2'):
+        TicTacToe[2] = ['mark']
+
+    elif(choice == 3 and TicTacToe[3] == '3'):
+        TicTacToe[3] = ['mark']
+
+    elif(choice == 4 and TicTacToe[4] == '4'):
+        TicTacToe[4] = ['mark']
+
+    elif(choice == 5 and TicTacToe[5] == '5'):
+        TicTacToe[5] = ['mark']
+
+    elif(choice == 6 and TicTacToe[6] == '6'):
+        TicTacToe[6] = ['mark']
+
+    elif(choice == 7 and TicTacToe[7] == '7'):
+        TicTacToe[7] = ['mark']
+
+    elif(choice == 8 and TicTacToe[8] == '8'):
+        TicTacToe[8] = ['mark']
+
+    elif(choice == 9 and TicTacToe[9] == '9'):
+        TicTacToe[9] = ['mark']
+
+    player = player + 1
+    print(TicTacToe)
