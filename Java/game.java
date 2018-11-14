cad3ac0c0e164b662e64d0e2b834f26e1ce6120a
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2018-11-13 20:45:10 -0500

    Create game.java

---
+++ Java/game.java
@@ -0,0 +1,33 @@
+public class game implements global {
+    
+    board gameBoard;
+    //player playerX;
+    //player playerY;
+    int turn;
+    
+    public game()
+    {
+        gameBoard.DisplayBoard();
+    }
+    
+    public void start()
+    {  
+    /*
+    turn X;
+    
+        if(turn == X)
+        {
+            playerX.play(gameBoard);
+            board.updateState;
+            playerY.play(gameBoard);
+        }
+        else
+        {
+            updateState();
+            playerY.play(gameBoard);
+            playerX.play(gameBoard);
+        }
+    */
+    }
+    
+}
