f27ab87c5b1482f6879d61e6d80902fd916b4a2c
Harshdip Singh Deogan <harshdipsdeogan@gmail.com>
2018-11-19 15:02:27 -0500

    Create HumanPlayer.java

---
+++ Java/HumanPlayer.java
@@ -0,0 +1,17 @@
+package tictactoenew;
+
+import java.util.Scanner;
+
+public class HumanPlayer extends player
+{
+    /*
+    public int blockInput()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter block to input your symbol: ");
+        int state = sc.nextInt();
+        
+        return state;        
+    }*/
+    
+}
